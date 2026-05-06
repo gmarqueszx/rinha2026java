78c582f98210a0069082fd3806e3a18ad03a3978
João Gabriel Marques <bielpesso@gmail.com>
2026-05-06 01:36:19 -0300

    first commit

---
+++ src/main/java/space/gmarqueszx/rinha_backend_2026/Rinhabackend2026Application.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.rinha_backend_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rinhabackend2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rinhabackend2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/gmarqueszx/rinha_backend_2026/Rinhabackend2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.rinha_backend_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rinhabackend2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
